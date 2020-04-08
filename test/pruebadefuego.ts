1724380026b8ae206c8b537ad1988d812ff73072
unknown <arandarenzogabriel@gmail.com>
2020-04-08 20:33:53 -0300

    version 10.2

--- test/pruebadefuego.ts
+++ test/pruebadefuego.ts
@@ -54,3 +54,70 @@ describe("calculadora", function(){
         expect(result.operar()).equal(100000000000000000000000);
     });
 })
+
+describe("calculadora", function(){
+    it("devolver 1 x 100 ceros",function(){
+
+        let aux = "";
+        
+        for (let i=0;i<100;i++){
+            aux = aux.concat("nueve")
+        }
+
+        let one  = new Context(aux);
+        one.reemplazarCadena();
+
+        let three = new Context("sumar");
+
+        let two = new Context ("uno");
+        two.reemplazarCadena();
+        
+
+        let var1 = new ExpresionNumero().interpreter(one);
+        let var2 = new ExpresionNumero().interpreter(two);
+        let operacion = new ExpresionOperacion().interpreter(three);
+
+        let result = new Calc(var1,var2,operacion);
+
+        let aux2 = "1";
+        for (let j=0;j<100;j++){
+            aux2 = aux2.concat("0")
+        }
+
+        expect(result.operar()).equal(parseInt(aux2));
+    });
+})
+
+describe("calculadora", function(){
+    it("devolver 1 x 1000 ceros",function(){
+
+        let aux = "";
+        
+        for (let i=0;i<1000;i++){
+            aux = aux.concat("nueve")
+        }
+
+        let one  = new Context(aux);
+        one.reemplazarCadena();
+
+        let three = new Context("sumar");
+
+        let two = new Context ("uno");
+        two.reemplazarCadena();
+        
+
+        let var1 = new ExpresionNumero().interpreter(one);
+        let var2 = new ExpresionNumero().interpreter(two);
+        let operacion = new ExpresionOperacion().interpreter(three);
+
+        let result = new Calc(var1,var2,operacion);
+
+        let aux2 = "1";
+        for (let j=0;j<1000;j++){
+            aux2 = aux2.concat("0")
+        }
+
+        expect(result.operar()).equal(parseInt(aux2));
+    });
+})
+
